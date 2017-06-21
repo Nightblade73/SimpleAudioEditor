d371afde744aaa087f54de728b7cb5a171da4a22
Nightblade73 <v-zar@list.ru>
2017-06-21 19:43:54 +0400

    Clearing from last project

--- SimpleAudioEditor/SimpleAudioEditor/View/MainForm.cs
+++ SimpleAudioEditor/SimpleAudioEditor/View/MainForm.cs
@@ -1,7 +1,4 @@
-﻿using CSCore;
-using CSCore.Codecs;
-using CSCore.SoundOut;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,7 +9,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SimpleAudioEditor.Controller;
-using CSCore.CoreAudioAPI;
 using System.Collections.ObjectModel;
 
 namespace SimpleAudioEditor
@@ -25,148 +21,45 @@ namespace SimpleAudioEditor
             InitializeComponent();
         }
 
-        private readonly ObservableCollection<MMDevice> mDevices = new ObservableCollection<MMDevice>();
-        private MMDeviceCollection mOutputDevices;
-
-        SoundSource soundSource = new SoundSource();
-        IWaveSource soundSource1;
-        //для загрузки ресурса(музыки) 2
-        IWaveSource soundSource2;
-        public static ISoundOut soundOut;
-        string fileSound;
-        List<string> fileSounds = new List<string>();
-
-        public List<Controller.WaveController.WaveEditor> WiveEditorList = new List<Controller.WaveController.WaveEditor>();
-        List<Button> EditorDeleteButtons = new List<Button>();
-
-
+       
         private void MainForm_Load(object sender, EventArgs e)
         {
-            if((new IntroForm(this).ShowDialog()) != DialogResult.OK)
-            {
-                this.Close();
-            }
-
-            trackBarVolume.Value = 30;
-
-            //Find sound capture devices and fill the cmbInput combo
-            MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
-            //Find sound render devices and fill the cmbOutput combo
-            MMDevice activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            mOutputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
-            foreach (MMDevice device in mOutputDevices)
-            {
-                comboBox1.Items.Add(device);
-                if (device.DeviceID == activeDevice.DeviceID) comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
-            }
-            comboBox1.DisplayMember = "FriendlyName";
+           
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            soundOut.Play();
+            
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (soundOut != null)
-            {
-                soundOut.Dispose();
-            }
-            if (mEditor != null)
-            {
-                mEditor.Dispose();
-                for (int i = 0; i < WiveEditorList.Count; i++)
-                {
-                    WiveEditorList[i].Dispose();
-                }
-            }
+            
         }
 
         private void buttonPause_Click(object sender, EventArgs e)
         {
-            soundOut.Pause();
+            
         }
 
         private void buttonLoadFile_Click(object sender, EventArgs e)
         {
-            openFileDialog.Filter = "Cursor Files|*.mp3";
-            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                for (int i = 0; i < openFileDialog.FileNames.Length; i++)
-                {
-                    fileSounds.Add(openFileDialog.FileNames[i]);
-                    WiveEditorList.Add(new Controller.WaveController.WaveEditor());
-                    if (WiveEditorList.Count <= 1)
-                        WiveEditorList[WiveEditorList.Count - 1].Location = new Point(0, 0);
-                    else
-                        WiveEditorList[WiveEditorList.Count - 1].Location = new Point(0, WiveEditorList[WiveEditorList.Count - 2].Location.Y + 66);
-
-                    WiveEditorList[WiveEditorList.Count - 1].Size = new Size( new Point(600, mEditor.Size.Height));
-                    
-                    this.Controls.Add(WiveEditorList[WiveEditorList.Count - 1]);
-                    WiveEditorList[WiveEditorList.Count - 1].Parent = samplesPanel;
-
-                    EditorDeleteButtons.Add(new Button());
-                    EditorDeleteButtons[EditorDeleteButtons.Count - 1].Text = "удалить";
-                    this.Controls.Add(EditorDeleteButtons[EditorDeleteButtons.Count - 1]);
-                    EditorDeleteButtons[EditorDeleteButtons.Count - 1].Location = new Point(
-                        WiveEditorList[WiveEditorList.Count - 1].Size.Width + 6, WiveEditorList[WiveEditorList.Count - 1].Location.Y);
-                    EditorDeleteButtons[EditorDeleteButtons.Count - 1].Parent = samplesPanel;
-                    EditorDeleteButtons[EditorDeleteButtons.Count - 1].Click+= buttonEditorDelete_Click;
-                    this.Update();
-                    //ресурс 1
-                    soundSource1 = soundSource.InitializationWaveSource(fileSounds[fileSounds.Count-1]);
-                    //теперь воспросизводить будем ресурс 1
-                     soundSource.InitializationSoundOut(soundSource1);
-                     soundOut.Volume = (float)0.3;
-                    try
-                    {
-                        WiveEditorList[WiveEditorList.Count - 1].OpenWaveFile(openFileDialog.FileNames[i], (MMDevice)comboBox1.SelectedItem);
-                        trackBarVolume.Value = WiveEditorList[WiveEditorList.Count - 1].Player.Volume;
-                        WiveEditorList[WiveEditorList.Count - 1].Focus();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Could not open file: " + ex.Message);
-                    }
-                }
-                MessageBox.Show("загружено");
-                WiveEditorList[WiveEditorList.Count - 1].Focus();
-            }
+           
         }
 
         private void buttonEditorDelete_Click(object sender, EventArgs e)//метод по осуществлению события клика
         {
-            int index = EditorDeleteButtons.IndexOf((Button)sender);
-            EditorDeleteButtons[index].Click -= buttonEditorDelete_Click;
-            EditorDeleteButtons[index].Dispose();
-            WiveEditorList[index].StopPlaying();
-            WiveEditorList[index].Dispose();
-            WiveEditorList.RemoveAt(index);
-            EditorDeleteButtons.RemoveAt(index);
-
-            RelocationEditorController();
+            
         }
 
         private void trackBarVolume_Scroll(object sender, EventArgs e)
         {
-            soundOut.Volume = (float)trackBarVolume.Value * 0.01f;
-            mEditor.Player.Volume = trackBarVolume.Value;
+           
         }
 
         private void RelocationEditorController()
         {
-            for(int i=0;i< WiveEditorList.Count; i++) { 
-            if (i == 0)
-                WiveEditorList[i].Location = new Point(0, 0);
-            else
-                WiveEditorList[i].Location = new Point(0, WiveEditorList[i - 1].Location.Y + 66);
-
-            EditorDeleteButtons[i].Location = new Point(
-                WiveEditorList[i].Size.Width + 6, WiveEditorList[i].Location.Y);
-            }
-            this.Update();
+            
         }
     }
 }
