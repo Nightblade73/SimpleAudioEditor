d8144384d5f3e8d7f8852bc6199259b69b724486
diller110 <diller110@mail.ru>
2017-06-23 23:52:36 +0400

    Add likely controller classes

---
+++ SimpleAudioEditor/SimpleAudioEditor/Controller/Project.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAudioEditor.Controller
+{
+    class Project
+    {
+        public String title;
+        public String path;
+        public Dictionary<int,Sample> samples;
+
+        /* Создает пустой проект
+         */
+        public Project()
+        {
+
+        }
+        /* Загрузить проект из папки path
+         */
+        public Project(String path)
+        {
+
+        }
+        /* Проиграть текущий проект
+         * int start    - секунда начала воспроизведения
+         * int stop     - секунда конца воспроизведения
+         */
+        public void Play(int start = 0, int stop = -1)
+        {
+
+        }
+        /* Добавляет в проект сэмпл(оригинал+сэмпл) в указанную позицию
+         */
+        public void AddSample(Sample s, int pos)
+        {
+
+        }
+        /* Удаляет из проекта сэмпл на указанной позиции
+         */
+        public void RemoveSample(Sample s, int pos)
+        {
+
+        }
+        /* Объеденить сэмплы в 1 муз. файл
+         * String filename  - название результирующего файла, сохранится по пути path
+         */
+        public void Combine(String filename)
+        {
+
+        }
+        /* Сохраняет проект в папку path
+         */
+        public void Save()
+        {
+
+        }
+    }
+}
---
+++ SimpleAudioEditor/SimpleAudioEditor/Controller/Sample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAudioEditor.Controller
+{
+    class Sample
+    {
+        public String title;
+        /* Путь к оригинальной звуковой дорожке
+         * (не обрезанной)
+         */
+        public String soundPath;
+        /* Путь к сэмплу (обрезанной звуковой дорожке)
+         * для каждого сэмпла, нужно создавать новый _экземпляр_ оригинальной дорожки
+         */
+        public String samplePath;
+        public int start;
+        public int stop;
+        
+        /* Воспроизвести оригинальную звуковую дорожку
+         */
+        public void Play()
+        {
+
+        }
+        /* Воспроизвести сэмпл, если указать start, stop обрезает и воспроизводит, но сэмпл не сохраняет
+         */
+        public void Play(int start = -1, int stop = -1)
+        {
+
+        }
+        /* Останавливает воспроизведение звука
+         */
+        public void Stop()
+        {
+
+        }
+        /* Обрезает оригинальную дорожку по указанным таймингам
+         */
+        public void Trim(int start, int stop)
+        {
+
+        }
+    }
+}
