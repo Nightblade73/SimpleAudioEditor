107d9083113947e1905d6e63df830575c594a20a
Fadki <zloizvyk@gmail.com>
2017-06-26 20:55:17 +0400

    добавлен класс XMLWorker

--- SimpleAudioEditor/SimpleAudioEditor/Controller/Primary.cs
+++ SimpleAudioEditor/SimpleAudioEditor/Controller/Primary.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SimpleAudioEditor.Controller
 {
@@ -67,5 +68,6 @@ namespace SimpleAudioEditor.Controller
             }
             return key;
         }
+
     }
 }
