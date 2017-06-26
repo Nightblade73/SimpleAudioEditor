42e8d5ef7dd57f2ee7516b8db378da46c6ec3de3
Fadki <zloizvyk@gmail.com>
2017-06-26 20:57:35 +0400

    добавил мелоч в XML

--- SimpleAudioEditor/SimpleAudioEditor/Controller/WorkerXML.cs
+++ SimpleAudioEditor/SimpleAudioEditor/Controller/WorkerXML.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SimpleAudioEditor.Controller
 {
     class WorkerXML
     {
+        public static void Serialize(Project project)
+        {
+
+        }
     }
 }
