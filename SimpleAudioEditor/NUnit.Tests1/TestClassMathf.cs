beedafbe68074cde80dd4cb840e27eec27418c22
Fadki <zloizvyk@gmail.com>
2017-06-29 16:08:06 +0400

    добавил описание тестов

--- SimpleAudioEditor/NUnit.Tests1/TestClassMathf.cs
+++ SimpleAudioEditor/NUnit.Tests1/TestClassMathf.cs
@@ -18,30 +18,43 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///есть отрезок (0;10) и точка А(50). 
+        ///если точка входит в отрезок, возвращается ее координата. 
+        ///если нет - координата конца отрезка, за которой лежит эта точка.
+        ///ожидаемый результат 10(последняя переменная)
+        ///т.к. 50 лежит вне отрезка, функция возвращает верхнюю границу - 10.
+        /// 10 = 10.положительный тест пройден
         public void TestMethodClamp1True()
         {
             Assert.AreEqual(Mathf.Clamp((int)50, (int)0, (int)10), 10);
         }
 
         [Test]
+        ///аналогичный тест, но на отрицание условия.
+        /// (0;10), А(50), ожидаемый результат - 50
+        /// т.к. функция вернет 10, то 10 != 50.
+        /// отрицательный тест пройден
         public void TestMethodClamp1False()
         {
             Assert.AreNotEqual(Mathf.Clamp((int)50, (int)0, (int)10), 50);
         }
 
         [Test]
+        ///аналогичный тест +
         public void TestMethodClamp2True()
         {
             Assert.AreEqual(Mathf.Clamp((double)4, (double)10, (double)50), 10);
         }
 
         [Test]
+        ///аналогичный тест - 
         public void TestMethodClamp2False()
         {
             Assert.AreNotEqual(Mathf.Clamp((double)4, (double)0, (double)50), 10);
         }
 
         [Test]
+        ///аналогичный тест +
         public void TestMethodClamp3True()
         {
 
@@ -49,6 +62,7 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///аналогичный тест -
         public void TestMethodClamp3False()
         {
 
@@ -56,6 +70,13 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///1 переменная - сколько секунд прошло (2)
+        ///2 переменная - сколько секунд всего (5)
+        ///3 переменная - сколько всего пикселей (20)
+        ///ожидаемый результат - сколкьо пикселей прошло (8)
+        /// позитивный тест. уравнение с одной неизвестной х = 20 / 5 * 2
+        /// х = 8
+        /// 8 = 8. тест пройден
         public void TestMethodTimeToPosTrue()
         {
 
@@ -63,6 +84,10 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///аналогичный тест -       
+        ///ожидаемый результат - сколкьо пикселей прошло (20)
+        ///20 != 8
+        ///тест пройден
         public void TestMethodTimeToPosFalse()
         {
 
@@ -70,6 +95,14 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///обратный тест.
+        ///1 переменная - сколько пикселей прошло (10)
+        ///2 переменная - сколько пикселей всего (40)
+        ///3 переменная - сколько всего секунд (4)
+        ///ожидаемый результат - сколкьо секунд прошло (1)
+        /// позитивный тест. уравнение с одной неизвестной х = 4 / 40 * 10 
+        /// х = 1
+        /// 1 = 1. тест пройден
         public void TestMethodPosToTimeTrue()
         {
 
@@ -77,6 +110,10 @@ namespace NUnit.Tests1
         }
 
         [Test]
+        ///аналогичный тест -   
+        ///ожидаемый результат - сколкьо секунд прошло (40)
+        ///1 != 40
+        ///тест пройден
         public void TestMethodPosToTimeFalse()
         {
 
